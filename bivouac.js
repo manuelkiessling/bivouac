b563b1e0c53f2ac4c81b52b2fdcbbb9bf19a6e8c
Manuel Kiessling <manuel@kiessling.net>
2012-02-23 15:54:56 +0100

    moved implementation to lib/

--- bivouac.js
+++ bivouac.js
@@ -1,11 +1,11 @@
 (function () {
-  var filesharing = require('./application/filesharing/filesharing.js');
-  var webserver = require('./application/webserver/webserver.js');
-  var chatserver = require('./application/chatserver/server/socketioserver.js');
-  var renderer = require('./application/chatserver/renderer/objectRenderer.js');
-  var connectionController = require('./application/chatserver/controller/connectionController.js');
+  var filesharing = require('./lib/application/filesharing/filesharing.js');
+  var webserver = require('./lib/application/webserver/webserver.js');
+  var chatserver = require('./lib/application/chatserver/server/socketioserver.js');
+  var renderer = require('./lib/application/chatserver/renderer/objectRenderer.js');
+  var connectionController = require('./lib/application/chatserver/controller/connectionController.js');
 
   connectionController.attachRenderer(renderer);
   connectionController.attachServer(chatserver);
-  chatserver.start(webserver.start(process.cwd() + '/application/client', filesharing.handleUpload, connectionController.handleUpload, filesharing.handleDownload));
+  chatserver.start(webserver.start(process.cwd() + '/lib/application/client', filesharing.handleUpload, connectionController.handleUpload, filesharing.handleDownload));
 })();
