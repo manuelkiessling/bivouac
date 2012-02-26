f399a19d38f94a61fd5577c3d8a5ffb79ffdfa06
Manuel Kiessling <manuel@kiessling.net>
2012-02-26 23:30:54 +0100

    rewrote room test to spec

---
+++ spec/server/domain/room.spec.js
@@ -0,0 +1,29 @@
+"use strict";
+var Room = require('../../../lib/server/domain/room.js').Room;
+
+var mocks = {
+  SystemCommunication: function(message) {
+    this.message = message;
+  }
+};
+var SystemCommunication =
+
+describe('addUser', function() {
+
+  it('adds a user to the room', function() {
+    var room = new Room('default', mocks.SystemCommunication);
+    var user = { name: 'foo' };
+    room.addUser(user);
+    expect(room.getUsers()[0]).toEqual(user);
+  });
+
+  it('creates a new system message', function() {
+    spyOn(mocks, 'SystemCommunication').andCallThrough();
+    var room = new Room('default', mocks.SystemCommunication);
+    room.addUser({ name: 'foo' });
+    expect(mocks.SystemCommunication).toHaveBeenCalledWith('User foo entered the chat.');
+    var communications = room.getCommunications();
+    expect(communications[0].message).toEqual('User foo entered the chat.');
+  });
+
+});
