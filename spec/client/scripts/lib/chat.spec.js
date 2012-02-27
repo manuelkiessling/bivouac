13b32dd01435ff0d32e75412a98d89e2199f08be
Manuel Kiessling <manuel@kiessling.net>
2012-02-27 09:47:44 +0100

    refactored chat lib to have socketio injected, wrote first spec

---
+++ spec/client/scripts/lib/chat.spec.js
@@ -0,0 +1,71 @@
+"use strict";
+var jsdom = require('jsdom').jsdom;
+var window = jsdom().createWindow();
+global.$ = global.jQuery = require('jQuery').create(window);
+
+var requirejs = require('requirejs');
+
+requirejs([__dirname + '/../../../../lib/client/scripts/lib/chat.js'], function(chat) {
+
+  describe('start', function() {
+
+    it('emits the "enter" event on the socket', function() {
+      var socket = {
+        emit: function(name, data) {}
+      }
+      var io = {
+        connect: function(host) {
+          return socket;
+        }
+      }
+
+      spyOn(socket, 'emit');
+      chat.start(io, 'johndoe');
+      expect(socket.emit).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('handleOutput', function() {
+
+    it('listens to the "socket.on hear" event', function() {
+      var mock = {
+        callback: function(communication) {}
+      }
+      var socket = {
+        on: function(name) {
+          if (name === 'hear') {
+            mock.callback();
+          }
+        }
+      }
+      spyOn(mock, 'callback');
+      chat.handleOutput(socket, null);
+      expect(mock.callback).toHaveBeenCalled();
+    });
+
+    it('appends the correct html if a user communication is received', function() {
+      var communication = {
+        type: 'user',
+        username: 'johndoe',
+        message: 'hello world'
+      }
+      var socket = {
+        on: function(name, callback) {
+          if (name === 'hear') callback(communication);
+        }
+      }
+      var messagesElement = {
+        append: function(content) {},
+        scrollTop: function(pixels) {}
+      }
+      spyOn(messagesElement, 'append');
+      spyOn(messagesElement, 'scrollTop');
+      chat.handleOutput(socket, messagesElement);
+      expect(messagesElement.append).toHaveBeenCalledWith('<div class="message user"><span class="says"><span class="username">johndoe</span> says:</span> hello world</div>');
+      expect(messagesElement.scrollTop).toHaveBeenCalledWith(999999);
+    });
+
+  });
+
+});
