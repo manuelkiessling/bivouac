633da5eae5319c104376262ba38d5fd3a85de0d9
Manuel Kiessling <manuel@kiessling.net>
2012-02-27 18:17:20 +0100

    better name for lib helper

--- spec/client/scripts/lib/helper.spec.js
+++ spec/client/scripts/lib/helper.spec.js
@@ -1,7 +1,7 @@
 "use strict";
 var requirejs = require('requirejs');
 
-requirejs([__dirname + '/../../../../lib/client/scripts/lib/common.js'], function(common) {
+requirejs([__dirname + '/../../../../lib/client/scripts/lib/helper.js'], function(helper) {
 
   describe('urlParam', function() {
 
@@ -11,7 +11,7 @@ requirejs([__dirname + '/../../../../lib/client/scripts/lib/common.js'], functio
           href: 'http://www.example.com/foo.bar?question=unknown&answer=42'
         }
       };
-      expect(common.urlParam('answer')).toEqual('42');
+      expect(helper.urlParam('answer')).toEqual('42');
     });
 
   });
