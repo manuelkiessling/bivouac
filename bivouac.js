4de4ff421f6168a4ad232d64e068bc516c16ee0d
Manuel Kiessling <manuel@kiessling.net>
2012-02-28 13:41:02 +0100

    refactoring multirooms into app

--- bivouac.js
+++ bivouac.js
@@ -3,10 +3,15 @@
   var filesharing = require('./lib/server/app/filesharing/filesharing.js');
   var httpd = require('./lib/server/app/httpd/httpd.js');
   var chatd = require('./lib/server/app/chatd/server/socketio.js');
-  var objectRenderer = require('./lib/server/app/chatd/renderer/object.js');
-  var connectionController = require('./lib/server/app/chatd/controller/connection.js');
+  var roomController = require('./lib/server/app/chatd/controller/room.js');
+  var connectionController = require('./lib/server/app/chatd/controller/room.js');
 
-  connectionController.attachRenderer(objectRenderer);
-  connectionController.attachServer(chatd);
-  chatd.start(httpd.start(process.cwd() + '/lib/client', filesharing.handleUpload, connectionController.handleUpload, filesharing.handleDownload));
+  roomController.init(
+    chatd.start(httpd.start(
+      process.cwd() + '/lib/client',
+      roomController.addRoom
+      )
+    ),
+    connectionController.create
+  );
 })();
--- spec/server/app/chatd/controller/room.spec.js
+++ spec/server/app/chatd/controller/room.spec.js
@@ -4,16 +4,61 @@ var roomController = require('../../../../../lib/server/app/chatd/controller/roo
 var mocks = {
   Room: function(name) {
     this.name = name;
-  }
+  },
+
+  chatdSocket: { of: function() { return 'namespaced socket' } },
+
+  attachRenderer: function(renderer) {},
+  attachRoomAndChatd: function(room, chatd) {},
+  handleUpload: function(filename, type) {}
 };
 
-describe('createRoom', function() {
+var connectionController = {
+    create: function() {
+      return {
+        attachRenderer: mocks.attachRenderer,
+        attachRoomAndChatd: mocks.attachRoomAndChatd,
+        handleUpload: mocks.handleUpload
+      }
+    }
+  }
 
-  it('adds a room to the roomlist', function() {
+describe('addRoom', function() {
+
+  it('does not add a room if it was not initialized', function() {
+    var success = roomController.addRoom('abcdefg', mocks.Room);
+    expect(success).toBeFalsy();
+  });
+
+  it('adds a room if it was initialized', function() {
+    roomController.init(mocks.chatdSocket, connectionController);
     spyOn(mocks, 'Room');
     var success = roomController.addRoom('abcdefg', mocks.Room);
     expect(mocks.Room).toHaveBeenCalledWith('abcdefg');
     expect(success).toBeTruthy();
+  });
+
+  it('adds several rooms', function() {
+    var success = roomController.addRoom('hijklmn', mocks.Room);
+    expect(success).toBeTruthy();
+    var success = roomController.addRoom('opqrstu', mocks.Room);
+    expect(success).toBeTruthy();
+    var success = roomController.addRoom('vwxyz', mocks.Room);
+    expect(success).toBeTruthy();
+  });
+
+  it('does not allow to create a room with an existing name', function() {
+    roomController.addRoom('abcdefg', mocks.Room);
+    var success = roomController.addRoom('abcdefg', mocks.Room);
+    expect(success).toBeFalsy();
+  });
+
+  it('sets up the connection controller for the new room', function() {
+    spyOn(connectionController, 'create').andCallThrough();
+    spyOn(mocks, 'attachRoomAndChatd');
+    roomController.addRoom('1234567890', mocks.Room);
+    expect(connectionController.create).toHaveBeenCalled();
+    expect(mocks.attachRoomAndChatd).toHaveBeenCalledWith({ name: '1234567890' }, 'namespaced socket');
   });
 
 });
