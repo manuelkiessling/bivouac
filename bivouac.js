94e6911d3d831364452c0e31487839a20ec2a500
Manuel Kiessling <manuel@kiessling.net>
2012-02-28 16:13:00 +0100

    changed because the chatd name changed

--- bivouac.js
+++ bivouac.js
@@ -3,7 +3,7 @@
   var domain = require('./lib/server/domain/domain.js');
   var filesharing = require('./lib/server/app/filesharing/filesharing.js');
   var httpd = require('./lib/server/app/httpd/httpd.js');
-  var chatd = require('./lib/server/app/chatd/server/socketio.js');
+  var chatd = require('./lib/server/app/chatd/daemon/chatd.js');
   var roomController = require('./lib/server/app/chatd/controller/room.js');
   var connectionController = require('./lib/server/app/chatd/controller/connection.js');
 
