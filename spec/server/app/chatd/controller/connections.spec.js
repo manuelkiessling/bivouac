6e0c52541fdbe8cd279f08177dc04ee390df9e66
Manuel Kiessling <manuel@kiessling.net>
2012-03-06 23:36:00 +0100

    specced that connectionsControllers must not provide the deliver() method

--- spec/server/app/chatd/controller/connections.spec.js
+++ spec/server/app/chatd/controller/connections.spec.js
@@ -6,6 +6,13 @@ describe('ConnectionsController', function() {
   it('creates connectionsController objects', function() {
     var connectionsController = new ConnectionsController();
     expect(typeof(connectionsController.attachRenderer)).toEqual('function');
+    expect(typeof(connectionsController.attachRoomAndChatd)).toEqual('function');
+    expect(typeof(connectionsController.sendDownloadCommunication)).toEqual('function');
+  });
+
+  it('does not provide objects where deliver() is public', function() {
+    var connectionsController = new ConnectionsController();
+    expect(typeof(connectionsController.deliver)).toEqual('undefined');
   });
 
 });
