3032af4be2e2154a8b35ffdf8a19d5bd6f95955c
Manuel Kiessling <manuel@kiessling.net>
2013-10-28 14:59:44 +0100

    Revert "changed fileupload spec"
    This reverts commit aff2dd1a1148d5b0ef270465899ef6be96f68d2b.

--- spec/client/scripts/lib/fileupload.spec.js
+++ spec/client/scripts/lib/fileupload.spec.js
@@ -1,23 +1,21 @@
 "use strict";
 var jsdom = require('jsdom').jsdom;
-jsdom.env('<html><body></body></html>', null, null,
-  function(errors, window) {
-    global.jQuery = require('jQuery').create(window);
-    
-    var requirejs = require('requirejs');
-    
-    requirejs([__dirname + '/../../../../src/client/scripts/lib/fileupload.js'], function(fileupload) {
-    
-      describe('Fileupload', function() {
-    
-        it('calls filedrop on dropElement', function() {
-          var dropElement = {};
-          dropElement.filedrop = createSpy("filedrop spy");
-          fileupload.initialize({ dropElement: dropElement });
-          expect(dropElement.filedrop).toHaveBeenCalled();
-        });
-    
-      });
-    
+var window = jsdom().createWindow();
+global.jQuery = require('jQuery').create(window);
+
+var requirejs = require('requirejs');
+
+requirejs([__dirname + '/../../../../src/client/scripts/lib/fileupload.js'], function(fileupload) {
+
+  describe('Fileupload', function() {
+
+    it('calls filedrop on dropElement', function() {
+      var dropElement = {};
+      dropElement.filedrop = createSpy("filedrop spy");
+      fileupload.initialize({ dropElement: dropElement });
+      expect(dropElement.filedrop).toHaveBeenCalled();
     });
+
+  });
+
 });
